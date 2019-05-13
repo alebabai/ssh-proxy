00b75bed45792b3c0918bcc950eafd5a8af4caeb
Alexander Babai <aliaksandr.babai@gmail.com>
2019-05-13 17:14:11 +0300

    refactor(script): add output file
    * use arrays instead of big string
    * add output file with tunnel urls

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -3,22 +3,27 @@
 function open_ssh_tunnel {
     local timestamp=`date +%s`
 
-    local tunnel_host=${SSH_TUNNEL_HOST:-serveo.net}
+    local tunnel_host=${SSH_TUNNEL_HOST:-"serveo.net"}
     local tunnel_prefix=${SSH_TUNNEL_PREFIX:-$timestamp}
-    local tunnel_port=${SSH_TUNNEL_PORT:-443}
-    local tunnel_bindings=""
+    local tunnel_port=${SSH_TUNNEL_PORT:-"443"}
+    local tunnel_bindings=()
+    local tunnel_urls=()
 
     for address in ${ADDRESS[@]}
     do
         local remote_hostname=$(echo $address | cut -d ':' -f 1)
         local tunnel_address=$tunnel_prefix-$remote_hostname:$tunnel_port
-        tunnel_bindings+=" -R $tunnel_address:$address"
+        tunnel_bindings+=("$tunnel_address:$address")
+        tunnel_urls+=("https://$tunnel_prefix-$remote_hostname.$tunnel_host")
     done
 
     if [ -z "$tunnel_bindings" ]; then
         exit 1
     else
-        local cmd="ssh -T -o ExitOnForwardFailure=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $SSH_ARGS $tunnel_bindings $tunnel_host"
+        local output_file=${OUTPUT_FILE:-"output.txt"}
+        printf "%s\n" ${tunnel_urls[@]} > $output_file
+        
+        local cmd="ssh -T -o ExitOnForwardFailure=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $SSH_ARGS $(printf " -R %s" ${tunnel_bindings[@]}) $tunnel_host"
         echo $cmd
         exec $cmd
     fi
